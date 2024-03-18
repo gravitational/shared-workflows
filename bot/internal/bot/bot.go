eb011895eba007a51ef9750c1e9d45c6b0fe1af6
Grzegorz Zdunek <grzegorz.zdunek@goteleport.com>
2024-03-18 10:24:53 +0100

    Ignore `protobuf-ts` client files during size check

--- bot/internal/bot/bot.go
+++ bot/internal/bot/bot.go
@@ -276,6 +276,7 @@ func skipFileForSizeCheck(name string) bool {
 		strings.HasSuffix(name, "_pb.ts") ||
 		strings.HasSuffix(name, "_pb.grpc-client.ts") ||
 		strings.HasSuffix(name, "_pb.grpc-server.ts") ||
+		strings.HasSuffix(name, "_pb.client.ts") ||
 		strings.HasSuffix(name, ".json") ||
 		strings.Contains(name, "webassets/") ||
 		strings.Contains(name, "vendor/") ||
