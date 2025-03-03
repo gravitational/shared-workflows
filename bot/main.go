d9d09d28847067529fad3e162feddc8522bd084c
Fred Heinecke <fred.heinecke@goteleport.com>
2025-03-03 16:01:23 -0600

    Contains -> HasPrefix

--- bot/main.go
+++ bot/main.go
@@ -149,7 +149,7 @@ func parseFlags() (flags, error) {
 
 	var decodedReviewers string
 	// Support base64-encoded JSON reviewer string and just JSON without base64 encoding
-	if strings.Contains(*reviewers, "{") {
+	if strings.HasPrefix(*reviewers, "{") {
 		decodedReviewers = *reviewers
 	} else {
 		reviewerBytes, err := base64.StdEncoding.DecodeString(*reviewers)
