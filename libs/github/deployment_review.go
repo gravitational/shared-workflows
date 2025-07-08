c1e14d3ce337074ea74aa0b5733cbc4877561170
Gus Rivera <gus.rivera@goteleport.com>
2025-07-08 16:52:19 -0500

    Use struct for parameters in deployment review call

--- libs/github/deployment_review.go
+++ libs/github/deployment_review.go
@@ -18,13 +18,26 @@ const (
 	PendingDeploymentApprovalStateRejected PendingDeploymentApprovalState = "rejected"
 )
 
+// ReviewDeploymentProtectionRuleInfo contains information about a deployment protection rule that is being reviewed.
+// This is used by GitHub Apps that are configured for environment protection rules.
+type ReviewDeploymentProtectionRuleInfo struct {
+	// RunID is the ID of the workflow run that is being reviewed.
+	RunID int64 `json:"run_id"`
+	// EnvironmentName is the name of the environment that is being reviewed.
+	EnvironmentName string `json:"environment_name"`
+	// State is the state of the deployment protection rule.
+	State PendingDeploymentApprovalState `json:"state"`
+	// Comment is an optional comment for the deployment protection rule.
+	Comment string `json:"comment,omitempty"`
+}
+
 // ReviewDeploymentProtectionRule reviews a deployment protection rule.
 // This is used by GitHub Apps that are configured for environment protection rules.
-func (c *Client) ReviewDeploymentProtectionRule(ctx context.Context, org, repo string, runID int64, state PendingDeploymentApprovalState, envName, comment string) error {
-	resp, err := c.client.Actions.ReviewCustomDeploymentProtectionRule(ctx, org, repo, runID, &github.ReviewCustomDeploymentProtectionRuleRequest{
-		State:           string(state),
-		EnvironmentName: envName,
-		Comment:         comment,
+func (c *Client) ReviewDeploymentProtectionRule(ctx context.Context, org, repo string, info ReviewDeploymentProtectionRuleInfo) error {
+	resp, err := c.client.Actions.ReviewCustomDeploymentProtectionRule(ctx, org, repo, info.RunID, &github.ReviewCustomDeploymentProtectionRuleRequest{
+		State:           string(info.State),
+		EnvironmentName: info.EnvironmentName,
+		Comment:         info.Comment,
 	})
 
 	if err != nil {
