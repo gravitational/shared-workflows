6351a70a860d6925e91800af0eefe147d957a7d2
Lukasz Okraszewski <lukasz.okraszewski@goteleport.com>
2026-01-06 18:08:21 +0000

    add ci-normalize tool

---
+++ tools/ci-normalize/cmd/ci-normalize.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	kingpin "github.com/alecthomas/kingpin/v2"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/dispatch"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/dispatch/adapter"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/encoder"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/input"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/meta"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/record"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/writer"
+	"github.com/gravitational/trace"
+)
+
+func makeWriter[T any](paths []string, format string, metadata *record.Meta) ([]dispatch.Option, error) {
+	var opts []dispatch.Option
+	for _, path := range paths {
+		raw, err := writer.New(path, metadata)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+
+		var enc encoder.Encoder
+		switch format {
+		case "jsonl":
+			enc = encoder.NewJSONLEncoder(raw)
+		default:
+			return nil, trace.BadParameter("unsupported format %q", format)
+		}
+
+		opts = append(opts, dispatch.WithWriter(new(T), adapter.New(enc, raw)))
+	}
+	return opts, nil
+}
+
+func makeDefaultWriter(format string) (dispatch.RecordWriter, error) {
+	raw, err := writer.New("-", nil)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	var enc encoder.Encoder
+	switch format {
+	case "jsonl":
+		enc = encoder.NewJSONLEncoder(raw)
+	default:
+		return nil, trace.BadParameter("unsupported format %q", format)
+	}
+
+	return adapter.New(enc, raw), nil
+}
+
+func setupDispatcher(format string, metadata *record.Meta, suiteOuts, testOuts, metaOuts []string) (*dispatch.Dispatcher, error) {
+	opts := []dispatch.Option{}
+
+	suiteOpts, err := makeWriter[record.Suite](suiteOuts, format, metadata)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	opts = append(opts, suiteOpts...)
+
+	testOpts, err := makeWriter[record.Testcase](testOuts, format, metadata)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	opts = append(opts, testOpts...)
+
+	metaOpts, err := makeWriter[record.Meta](metaOuts, format, metadata)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	opts = append(opts, metaOpts...)
+
+	defWriter, err := makeDefaultWriter(format)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	opts = append(opts, dispatch.WithDefaultWriter(defWriter))
+
+	d, err := dispatch.New(opts...)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return d, nil
+}
+
+func createProducers(cmd string, junitCmd *kingpin.CmdClause, metadata *record.Meta, junitFiles *[]string) ([]input.Producer, error) {
+	producers := []input.Producer{input.NewPassthroughProducer(metadata)}
+	opts := []input.Option{input.WithMeta(metadata)}
+
+	switch cmd {
+	case junitCmd.FullCommand():
+		for _, f := range *junitFiles {
+			p, err := input.NewJUnitProducer(f, opts...)
+			if err != nil {
+				return nil, trace.Wrap(err)
+			}
+			producers = append(producers, p)
+		}
+	case "meta":
+		// No additional producers needed
+	default:
+		return nil, trace.NotImplemented("unimplemented command %q", cmd)
+	}
+
+	if len(producers) == 0 {
+		return nil, trace.BadParameter("nothing to do")
+	}
+
+	return producers, nil
+}
+
+func run() error {
+	ctx := context.TODO()
+	app := kingpin.New("ci-normalize", "Normalize test artifacts")
+	app.HelpFlag.Short('h')
+	format := app.Flag("format", "Output format").Short('f').Default("jsonl").Enum("jsonl")
+	metaOuts := app.Flag(
+		"meta",
+		"Metadata output ('-' for stdout, /dev/null to ignore)",
+	).Short('o').Default("-").Strings()
+
+	// metadata command
+	app.Command("meta", "Emit metadata")
+
+	// JUnit command
+	junitCmd := app.Command("junit", "Normalize JUnit test results")
+	suiteOuts := junitCmd.Flag("suites", "Testsuite output(s) ('-' for stdout, /dev/null to ignore)").Default("-").Strings()
+	testOuts := junitCmd.Flag("tests", "Testcase output(s) ('-' for stdout, /dev/null to ignore)").Default("-").Strings()
+	junitFiles := junitCmd.Arg("files", "JUnit XML result files").Required().ExistingFiles()
+	metaFile := junitCmd.Flag("from-meta", "Optionally provide existing metadata").ExistingFile()
+
+	cmd, err := app.Parse(os.Args[1:])
+	if err != nil {
+		return trace.Wrap(err, "failed to parse command line arguments")
+
+	}
+
+	metadata, err := meta.New(metaFile)
+	if err != nil {
+		return trace.Wrap(err, "reading metadata")
+	}
+
+	dispatcher, err := setupDispatcher(*format, metadata, *suiteOuts, *testOuts, *metaOuts)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	defer dispatcher.Close()
+
+	producers, err := createProducers(cmd, junitCmd, metadata, junitFiles)
+	if err != nil {
+		return trace.Wrap(err)
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	for _, p := range producers {
+		p := p // capture
+		eg.Go(func() error {
+			return p.Produce(ctx, dispatcher.Write)
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return trace.Wrap(err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ tools/ci-normalize/pkg/meta/github.go
@@ -0,0 +1,81 @@
+package meta
+
+import (
+	"strings"
+	"time"
+
+	"github.com/caarlos0/env/v9"
+	"github.com/gravitational/shared-workflows/tools/ci-normalize/pkg/record"
+	"github.com/gravitational/trace"
+)
+
+type GithubMeta struct {
+	// Required for canonical ids:
+	Repository   string `env:"GITHUB_REPOSITORY,notEmpty"`
+	RunID        string `env:"GITHUB_RUN_ID,notEmpty"`
+	RunNumber    int    `env:"GITHUB_RUN_NUMBER,notEmpty"`
+	RunAttempt   int    `env:"GITHUB_RUN_ATTEMPT,notEmpty"`
+	JobName      string `env:"GITHUB_JOB,notEmpty"`
+	WorkflowName string `env:"GITHUB_WORKFLOW,notEmpty"`
+	GitSHA       string `env:"GITHUB_SHA,notEmpty"`
+
+	GitRef            string `env:"GITHUB_REF"`
+	GitRefName        string `env:"GITHUB_REF_NAME"`
+	BaseRef           string `env:"GITHUB_BASE_REF"`
+	HeadRef           string `env:"GITHUB_HEAD_REF"`
+	GithubActor       string `env:"GITHUB_ACTOR"`
+	GithubActorID     string `env:"GITHUB_ACTOR_ID"`
+	RunnerArch        string `env:"RUNNER_ARCH"`
+	RunnerOS          string `env:"RUNNER_OS"`
+	RunnerName        string `env:"RUNNER_NAME"`
+	RunnerEnvironment string `env:"RUNNER_ENVIRONMENT"`
+}
+
+func newFromGithubEnv() (*record.Meta, error) {
+	now := time.Now()
+	var gh GithubMeta
+	if err := env.Parse(&gh); err != nil {
+		return nil, trace.Wrap(err, "could not read Github metadata from env")
+	}
+
+	canonical := record.CanonicalMeta{
+		CanonicalMetaSchemaVersion: record.CanonicalMetaSchemaVersion,
+		Provider:                   "github.com",
+		RepositoryName:             strings.ToLower(gh.Repository),
+		Workflow:                   strings.ToLower(gh.WorkflowName),
+		Job:                        strings.ToLower(gh.JobName),
+		RunID:                      gh.RunID,
+		RunAttempt:                 gh.RunAttempt,
+		SHA:                        strings.ToLower(gh.GitSHA),
+	}
+
+	id, err := canonical.Id()
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+
+	return &record.Meta{
+		Common: record.Common{
+			ID:                  id,
+			RecordSchemaVersion: record.RecordSchemaVersion,
+		},
+		CanonicalMeta: canonical,
+		GitMeta: record.GitMeta{
+			GitRef:     gh.GitRef,
+			GitRefName: gh.GitRefName,
+			BaseRef:    gh.BaseRef,
+			HeadRef:    gh.HeadRef,
+		},
+		ActorMeta: record.ActorMeta{
+			Actor:   gh.GithubActor,
+			ActorID: gh.GithubActorID,
+		},
+		RunnerMeta: record.RunnerMeta{
+			RunnerArch:        gh.RunnerArch,
+			RunnerOS:          gh.RunnerOS,
+			RunnerName:        gh.RunnerName,
+			RunnerEnvironment: gh.RunnerEnvironment,
+		},
+		Timestamp: now.Format(time.RFC3339),
+	}, nil
+}
