f03490d3090b3ca822766d86b90d717d80537abe
Matt Smith <matt.smith@goteleport.com>
2026-08-07 14:24:41 -0400

    Refactor changelog tool to support new release process (#493)
    * Refactor changelog tool to use graphql api
    
    * Go mod updates

--- tools/changelog/go.mod
+++ tools/changelog/go.mod
@@ -4,23 +4,34 @@ go 1.26
 
 require (
 	github.com/alecthomas/kingpin/v2 v2.4.0
-	github.com/gravitational/shared-workflows/libs v0.1.6
+	github.com/gravitational/shared-workflows/libs v0.1.8
 	github.com/gravitational/trace v1.5.1
 	github.com/stretchr/testify v1.10.0
 )
 
 require (
 	github.com/alecthomas/units v0.0.0-20240927000941-0f3dac36c52b // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/cli/go-gh/v2 v2.12.1 // indirect
 	github.com/cli/safeexec v1.0.1 // indirect
+	github.com/cli/shurcooL-graphql v0.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/go-github/v84 v84.0.0 // indirect
 	github.com/google/go-querystring v1.2.0 // indirect
+	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
 	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
+	golang.org/x/term v0.43.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tools/changelog/main.go
+++ tools/changelog/main.go
@@ -17,14 +17,10 @@ limitations under the License.
 package main
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"log"
-	"os/exec"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/gravitational/trace"
@@ -33,78 +29,66 @@ import (
 	"github.com/gravitational/shared-workflows/libs/github"
 )
 
-var (
-	baseBranch = kingpin.Flag(
-		"base-branch",
-		"The base release branch to generate the changelog for. It will be of the form branch/v*",
-	).Envar("BASE_BRANCH").String()
-
-	baseTag = kingpin.Flag(
-		"base-tag",
-		"The tag/version to generate the changelog from. It will be of the form vXX.Y.Z, e.g. v15.1.1",
-	).Envar("BASE_TAG").String()
-
-	dir = kingpin.Arg("dir", "directory of the teleport repo.").Required().String()
-)
-
 func main() {
+	var (
+		baseBranch = kingpin.Flag(
+			"base-branch",
+			"The base release branch to generate the changelog for, of the form branch/v*.",
+		).Envar("BASE_BRANCH").Required().String()
+
+		baseTag = kingpin.Flag(
+			"base-tag",
+			"The tag/version to generate the changelog from, of the form vXX.Y.Z, e.g. v15.1.1.",
+		).Envar("BASE_TAG").Required().String()
+
+		dir = kingpin.Arg("dir", "The directory of the teleport repo.").Required().String()
+	)
 	kingpin.Parse()
 
-	ossRepo, entRepo, err := initGit(*dir)
-	if err != nil {
+	if err := run(context.Background(), *dir, *baseBranch, *baseTag); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context, dir, baseBranch, baseTag string) error {
+	ossRepo := git.NewRepo(dir)
+	entRepo := git.NewRepo(filepath.Join(dir, "e"))
+
+	// The enterprise repo is a submodule of the OSS repo, so resolve the
+	// enterprise refs from the SHAs the OSS refs point the submodule at.
+	eBranchSHA, err := ossRepo.ObjectSHAAtPath(baseBranch, "e")
+	if err != nil {
+		return trace.Wrap(err)
+	}
+	eTagSHA, err := ossRepo.ObjectSHAAtPath(baseTag, "e")
+	if err != nil {
+		return trace.Wrap(err)
 	}
 
-	// Figure out the branch and last version released for that branch
-	branch, err := getBranch(*baseBranch, ossRepo)
+	ossPRs, err := ossRepo.PRsBetweenRefs(baseTag, baseBranch)
 	if err != nil {
-		log.Fatal(err)
+		return trace.Wrap(err)
+	}
+	entPRs, err := entRepo.PRsBetweenRefs(eTagSHA, eBranchSHA)
+	if err != nil {
+		return trace.Wrap(err)
 	}
 
-	lastVersion, err := getLastVersion(*baseTag, *dir)
+	gh, err := github.NewClientFromGHAuth(ctx)
 	if err != nil {
-		log.Fatal(trace.Wrap(err, "failed to determine last version"))
+		return trace.Wrap(err)
 	}
 
-	// Determine timestamps of releases which is used to limit Github search
-	timeLastRelease, err := ossRepo.TimestampForRef(lastVersion)
-	if err != nil {
-		log.Fatal(err)
-	}
-	timeLastEntRelease, timeLastEntMod, err := entTimestamps(entRepo, lastVersion)
-	if err != nil {
-		log.Fatal(err)
-	}
+	ossGen := &generator{gh: gh, repo: "teleport", tmpl: tmplLinks}
+	entGen := &generator{gh: gh, repo: "teleport.e", tmpl: tmplNoLinks}
 
-	cl, err := github.NewClientFromGHAuth(context.Background())
+	ossCL, err := ossGen.generate(ctx, ossPRs)
 	if err != nil {
-		log.Fatal(err)
+		return trace.Wrap(err)
 	}
-
-	// Bump the time of the last release (both OSS and Enterprise) by 1 second
-	// as we do not want to include the commit in the last release.
-	timeLastRelease = timeLastRelease.Add(1 * time.Second)
-	timeLastEntRelease = timeLastEntRelease.Add(1 * time.Second)
-	timeLastEntMod = timeLastEntMod.Add(1 * time.Second)
-
-	// Generate changelogs
-	ctx := context.Background()
-	ossCLGen := &changelogGenerator{
-		ghclient: cl,
-		repo:     "teleport",
-	}
-	entCLGen := &changelogGenerator{
-		ghclient:       cl,
-		repo:           "teleport.e",
-		excludePRLinks: true,
-	}
-	ossCL, err := ossCLGen.generateChangelog(ctx, branch, timeLastRelease, github.SearchTimeNow)
+	entCL, err := entGen.generate(ctx, entPRs)
 	if err != nil {
-		log.Fatal(err)
-	}
-	entCL, err := entCLGen.generateChangelog(ctx, branch, timeLastEntRelease, timeLastEntMod)
-	if err != nil {
-		log.Fatal(err)
+		return trace.Wrap(err)
 	}
 
 	fmt.Println(ossCL)
@@ -112,91 +96,6 @@ func main() {
 		fmt.Println("Enterprise:")
 		fmt.Println(entCL)
 	}
-}
 
-// initGit will initialize git repo for teleport OSS and Enterprise.
-func initGit(repoRootDir string) (oss, ent *git.Repo, err error) {
-	oss, err = git.NewRepoFromDirectory(repoRootDir)
-	if err != nil {
-		return oss, ent, trace.Wrap(err)
-	}
-
-	ent, err = git.NewRepoFromDirectory(filepath.Join(repoRootDir, "e"))
-	if err != nil {
-		return oss, ent, trace.Wrap(err)
-	}
-	return
-}
-
-// getBranch will return branch if parsed otherwise will attempt to find it
-// Branch should be in the format "branch/v*"
-func getBranch(branch string, repo *git.Repo) (string, error) {
-	if branch != "" {
-		return branch, nil
-	}
-
-	branch, err := repo.BranchNameForHead()
-	if err != nil {
-		return "", trace.Wrap(err)
-	}
-
-	// if the branch is not in the branch/v* format then check it's root
-	if !strings.HasPrefix(branch, "branch/v") {
-		fbranch, err := repo.GetParentReleaseBranch()
-		if err != nil {
-			return "", trace.Wrap(err, "could not determine a root branch")
-		}
-		branch = fbranch
-	}
-
-	return branch, nil
-}
-
-func getLastVersion(baseTag, dir string) (string, error) {
-	if baseTag != "" {
-		return baseTag, nil
-	}
-
-	lastVersion, err := makePrintVersion(dir)
-	if err != nil {
-		return "", trace.Wrap(err, "base-tag was not set, defaulted to invoking 'make version' but failed")
-	}
-
-	return lastVersion, nil
-}
-
-// makePrintVersion will run 'make -s print-version'
-func makePrintVersion(dir string) (string, error) {
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-
-	cmd := exec.Command("make", "-s", "print-version")
-	cmd.Dir = dir
-	cmd.Stderr = &stderr
-	cmd.Stdout = &stdout
-
-	err := cmd.Run()
-
-	if err != nil {
-		return strings.TrimSpace(stderr.String()), trace.Wrap(err, "can't get last released version")
-	}
-	out := strings.TrimSpace(stdout.String())
-
-	return "v" + out, nil
-}
-
-func entTimestamps(entRepo *git.Repo, tag string) (lastRelease, lastCommit time.Time, err error) {
-	// get timestamp since last release
-	lastRelease, err = entRepo.TimestampForRef(tag)
-	if err != nil {
-		return lastRelease, lastCommit, trace.Wrap(err, "can't get commit for tag")
-	}
-
-	// get timestamp of last commit
-	lastCommit, err = entRepo.TimestampForLatestCommit()
-	if err != nil {
-		return lastRelease, lastCommit, trace.Wrap(err, "can't get timestamp for ent")
-	}
-
-	return lastRelease, lastCommit, nil
+	return nil
 }
